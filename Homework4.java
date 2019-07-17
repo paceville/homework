84327af1a5823ae87cfb76c25284b524b2c909a3
Elena <52384305+paceville@users.noreply.github.com>
2019-07-17 19:20:11 +0300

    Update Homework4.java

--- Homework4.java
+++ Homework4.java
@@ -1,32 +1,35 @@
-package com.company;
+//package com.company;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
+    public class Homework4 {
+        public static void main(String[] args) {
 
-public class Homework4 {
-    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("How many grooms you will sort? ");
+            int amount;
+            amount = input.nextInt();
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("How many grooms you will sort? ");
-        int amount;
-        amount = input.nextInt();
+            Man man = new Man();
 
-        ArrayList<Man> groom = new ArrayList<>()
-        //Man[] groom = new Man [amount];
+            ArrayList<Man> groom = new ArrayList<>();
+            //Man[] groom = new Man [amount];
 
-        for (int i = 0; i <= amount; i++){
-            System.out.println("Input groom`s name ");
-            String groom_name = input.next();
-            groom[i] = new Man(groom_name);
-        }
+            for (int i = 0; i <= amount; i++){
+                System.out.println("Input groom`s name: ");
+                man.name = input.next();
+                groom.set(i, create(man.name));
+            }
 
 
         }
 
     }
 
-class Man {
-    String name;
-    int iq;
+    class Man {
+        String name;
+        int iq;
+    }
+    
+    public static create
 }
