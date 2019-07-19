8e6904a1e94620f6a9b51c9b5e5b1d37743cddd3
Elena <52384305+paceville@users.noreply.github.com>
2019-07-20 00:41:39 +0300

    Update Homework4.java

--- Homework4.java
+++ Homework4.java
@@ -1,6 +1,6 @@
 package com.company;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -18,24 +18,49 @@ public class Homework4 {
         ArrayList<Man> groom = new ArrayList<>();
         //Man[] groom = new Man [amount];
 
-        for (int i = 0; i < amount; i++){
+        Man man = new Man();
+
+        for (int i = 0; i < amount; i++) {
             System.out.println("Input groom`s name ");
-            Man man = new Man();
             man.name = input.next();
             man.iq = 30 + random.nextInt(90);
             groom.add(i, (man));
-            //System.out.println("Name: " + man.name + "`s IQ " + man.iq);
-            
-            for ()
-        }
-        
+            System.out.println("Name: " + man.name + "`s IQ " + man.iq);
 
         }
 
+        for (int a = 1; a < groom.size(); a++){
+            for (int i = 0; i < groom.size()-1; i++) {
 
-}
+                Man left = groom.get(i);
+                Man right = groom.get(i + 1);
 
-class Man {
-    java.lang.String name;
-    int iq;
+                if (left.iq < right.iq) {
+                    left = groom.get(i + 1);
+                    right = groom.get(i);
+                    System.out.println("left = " + left);
+                    System.out.println("right = " + right);
+                }
+                //System.out.println("left = " + left);
+                //System.out.println("right = " + right);
+                //System.out.println(groom.get(i));
+                //System.out.println(man.iq);
+            }
+        }
+
+        System.out.println("How many grooms you will see on the top?");
+
+        int top = input.nextInt();
+
+        //for (int candidates = 0; candidates <= top; candidates++) ;
+
+
+
+
+    }
+
+    static class Man {
+        java.lang.String name;
+        int iq;
+    }
 }
