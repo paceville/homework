f864ca997f4a11ab81d2cf8b39986f79d97dab4c
Elena <52384305+paceville@users.noreply.github.com>
2019-07-19 17:40:58 +0300

    Update Homework4.java

--- Homework4.java
+++ Homework4.java
@@ -1,35 +1,41 @@
-//package com.company;
+package com.company;
+
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
-    public class Homework4 {
-        public static void main(String[] args) {
 
-            Scanner input = new Scanner(System.in);
-            System.out.print("How many grooms you will sort? ");
-            int amount;
-            amount = input.nextInt();
 
+public class Homework4 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many grooms you will sort? ");
+        int amount;
+        amount = input.nextInt();
+        Random random = new Random();
+
+        ArrayList<Man> groom = new ArrayList<>();
+        //Man[] groom = new Man [amount];
+
+        for (int i = 0; i < amount; i++){
+            System.out.println("Input groom`s name ");
             Man man = new Man();
-
-            ArrayList<Man> groom = new ArrayList<>();
-            //Man[] groom = new Man [amount];
-
-            for (int i = 0; i <= amount; i++){
-                System.out.println("Input groom`s name: ");
-                man.name = input.next();
-                groom.set(i, create(man.name));
-            }
-
+            man.name = input.next();
+            man.iq = 30 + random.nextInt(90);
+            groom.add(i, (man));
+            //System.out.println("Name: " + man.name + "`s IQ " + man.iq);
+            
+            for ()
+        }
+        
 
         }
 
-    }
 
-    class Man {
-        String name;
-        int iq;
-    }
-    
-    public static create
+}
+
+class Man {
+    java.lang.String name;
+    int iq;
 }
