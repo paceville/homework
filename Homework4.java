bb34e84bf3415526e42febccbcde98e04353bd6f
Elena <52384305+paceville@users.noreply.github.com>
2019-07-21 23:55:36 +0300

    Update Homework4.java
    IT WORKS!!!!!!

--- Homework4.java
+++ Homework4.java
@@ -16,21 +16,16 @@ public class Homework4 {
         Random random = new Random();
 
         ArrayList<Man> groom = new ArrayList<>();
-        //Man[] groom = new Man [amount];
-
-        Man man = new Man();
 
         for (int i = 0; i < amount; i++) {
+            Man man = new Man();
             System.out.println("Input groom`s name ");
             man.name = input.next();
             man.iq = 30 + random.nextInt(90);
             groom.add(i, (man));
-            System.out.println("Name: " + man.name + "`s IQ " + man.iq);
+            System.out.println(Arrays.toString(new ArrayList[]{groom}));
 
         }
-
-        // проблема: в топ выводится n раз последний кандидат. проблема с элементами списка? 
-
 
         for (int a = 1; a < groom.size(); a++) {
             for (int i = 0; i < groom.size() - a; i++) {
@@ -38,41 +33,42 @@ public class Homework4 {
                 Man right = groom.get(i + 1);
 
                 if (left.iq < right.iq) {
+
                     int temp = left.iq;
-                    left = groom.get(i + 1);
-                    right = groom.get(i);
-                    System.out.println("left = " + left);
-                    System.out.println("right = " + right);
+                    left.iq = right.iq;
+                    right.iq = temp;
+
+//                    System.out.println("temp = " + temp);
+//                    System.out.println("left = " + left);
+//                    System.out.println("right = " + right);
+//                    System.out.println(Arrays.toString(new ArrayList[]{groom}));
+
                 }
-                //System.out.println("left = " + left);
-                //System.out.println("right = " + right);
-                //System.out.println(groom.get(i));
-                System.out.println(man.iq);
+
             }
         }
+        System.out.println(Arrays.toString(new ArrayList[]{groom}));
 
         System.out.println("How many grooms you will see on the top?");
 
         int top = input.nextInt();
 
-        //for (int candidates = 1; candidates <= top; candidates++) {
-            for (int i = 0; i < groom.size(); i++)
-            //groom.get(candidates);
+        for (int i = 0; i < top; i++) {
+
             System.out.println(groom.get(i));
-            //55System.out.println(Arrays.toString(new ArrayList[]{groom}));
         }
 
     }
 
+}
 
 
+class Man {
+    java.lang.String name;
+    int iq;
 
-      class Man {
-          java.lang.String name;
-          int iq;
-
-          @Override
-          public String toString(){
-              return name + "`s IQ is " + iq;
-          }
-      }
+    @Override
+    public String toString(){
+        return name + "`s IQ is " + iq;
+    }
+}
